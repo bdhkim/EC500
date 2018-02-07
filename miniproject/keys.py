3694e6e06528f12af6190cc7743595d8a8b3f0ef
bdhkim <bdhkim@bu.edu>
2018-02-07 01:03:30 -0500

    Tweepy
    Tweepy OAuth

---
+++ miniproject/keys.py
@@ -0,0 +1,6 @@
+
+
+ckey = 'trJCiIv8NHIuUCcFyXomRcLa0'
+csecret = 'pCKKyezKHR8Sqosq5gjy8qprTYpudUQUwBpKh347DpmgeUrd9g'
+akey = '1067492610-0fgbJo1DjhGvJAVgxdV93wE0XCcDipg8ciVdHQf'
+asecret = 'Q7v0oAJJUnj4xJl7oGyxOf8kEFmpJiMh3DBB3mcXrzgOB'
